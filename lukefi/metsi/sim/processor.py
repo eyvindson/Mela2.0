7beef8bb046ae52da3fbe5dfc9c8c60ca442ca5c
Tapani Stylman <131968101+TStylman@users.noreply.github.com>
2025-11-25 13:19:34 +0200

    feat: transition, #MELA2-61 (#19)
    ### Replaced fixed time points with configurable transition
    functionality
    
    Instead of pre-defining events at specific time points, the simulation
    now advances the state in time via a configurable transition function.
    Events are now checked by condition after each transition and carried
    out every time the conditions are met.
    
    A special end condition is also provided, that, upon being met, ends the
    simulation for current branch.

--- lukefi/metsi/sim/processor.py
+++ lukefi/metsi/sim/processor.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import TYPE_CHECKING
 from lukefi.metsi.app.utils import ConditionFailed
 from lukefi.metsi.data.computational_unit import ComputationalUnit
@@ -11,34 +12,33 @@ if TYPE_CHECKING:
 def processor[T: ComputationalUnit](payload: SimulationPayload[T],
                                     operation: "TreatmentFn[T]",
                                     operation_tag: "TreatmentFn[T]",
-                                    time_point: int,
                                     preconditions: list[Condition[SimulationPayload[T]]],
                                     postconditions: list[Condition[SimulationPayload[T]]],
-                                    **operation_parameters: dict[str,
-                                                                 dict]) -> tuple[SimulationPayload[T],
-                                                                                 list[CollectedData]]:
+                                    **operation_parameters: dict[str, dict]) -> tuple[SimulationPayload[T],
+                                                                                      list[CollectedData]]:
     """Managed run conditions and history of a simulator operation. Evaluates the operation."""
     for condition in preconditions:
-        if not condition(time_point, payload):
+        if not condition(payload):
             raise ConditionFailed(f'{operation_tag} aborted - condition "{condition}" failed')
 
     try:
         new_state, new_collected_data = operation(payload.computational_unit)
     except UserWarning as e:
         raise UserWarning(f"Unable to perform operation {operation_tag}, "
-                          f"at time point {time_point}; reason: {e}") from e
+                          f"at time point {payload.computational_unit.time}; reason: {e}") from e
 
     new_state.update_aggregates()
 
     newpayload: SimulationPayload[T] = SimulationPayload(
         computational_unit=new_state,
-        operation_history=payload.operation_history
+        operation_history=payload.operation_history,
+        node_id=deepcopy(payload.node_id)
     )
 
     for condition in postconditions:
-        if not condition(time_point, newpayload):
+        if not condition(newpayload):
             raise ConditionFailed(f'{operation_tag} aborted - condition "{condition}" failed')
 
-    newpayload.operation_history.append((time_point, operation_tag, operation_parameters))
+    newpayload.operation_history.append((payload.computational_unit.time, operation_tag, operation_parameters))
 
     return newpayload, new_collected_data
